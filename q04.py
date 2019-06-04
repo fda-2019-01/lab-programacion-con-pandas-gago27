324913dfe81536a38e04dcec67294ecc71733f67
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-06-03 20:40:04 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -2,5 +2,14 @@
 ## Imprima los valores unicos e la columna _c4 de 
 ## de la tabla tbl1 en mayusculas
 ## 
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
+tbl_1 = pd.read_csv('tbl1.tsv',
+                   sep = '\t',
+                   thousands = None,
+                   decimal = '.')
+tbl_1 = tbl_1.sort_values(by=['_c4'])
+tbl_1['_c4'] = tbl_1['_c4'].map(lambda x: x.upper())
 
-
+tbl_1._c4.unique()
