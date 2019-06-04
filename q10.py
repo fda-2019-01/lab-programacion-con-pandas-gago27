d2f08cae8735a9d1c537ac3be85f7bea1fe9a4e2
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-06-03 20:43:27 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,27 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
+tbl1 = pd.read_csv('tbl1.tsv',
+                  sep = '\t',
+                  thousands = None,
+                  decimal = '.')
+tbl_1 = tbl1.groupby('_c0')
+tbl = tbl_1.groups.keys()
 
+listas = []
+for i in tbl:
+  j = tbl1.loc[tbl_1.groups[i]]
+  k = list(j['_c4'])
+  listas = listas +[k]
+lista = []
+for j in listas:
+  j = ','.join(j)
+  lista = lista+[j]
+lista
+
+respuesta = pd.DataFrame({'_c0' : list(tbl),
+                         'lista' : list(lista)})
+respuesta
