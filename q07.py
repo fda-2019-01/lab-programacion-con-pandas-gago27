40b206cfbedeece98edcdd0e30cf2de541d91c2e
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-06-03 20:42:10 -0500

    Update q07.py

--- q07.py
+++ q07.py
@@ -1,4 +1,12 @@
 ##
 ## Agregue una columna  con la suma de _c0 y _c2 a la tabla tbl0.tsv 
 ## 
-
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
+tbl_0 = pd.read_csv('tbl0.tsv',
+                sep = '\t',
+                thousands = None,
+                decimal = '.')
+tbl_0['suma']= tbl_0['_c0']+ tbl_0['_c2']
+tbl_0
