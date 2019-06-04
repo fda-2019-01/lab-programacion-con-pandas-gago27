80eab2037bbb6d09b98d2a95b62be4c95997d923
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-06-03 20:45:01 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -3,5 +3,21 @@
 ## y tbl2.tsv, compute la suma de tbl2._c5b por cada
 ## valor en tbl0._c1.
 ## 
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
 
+tbl2 = pd.read_csv('tbl2.tsv',
+                  sep = '\t',
+                  thousands = None,
+                  decimal = '.')
+tbl_2 = tbl2.groupby('_c5a').sum()
+suma = list(tbl_2['_c5b'])
+suma = [str(line) for line in suma]
+tbl = tbl2.groupby('_c5a')
+c5a = tbl.groups.keys()
 
+respuesta = pd.DataFrame({'_c5a' : list(c5a),
+                         '' : list(suma)})
+
+respuesta
