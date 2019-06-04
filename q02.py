620779e9ba3ef9238dbf8892dcdb3a762c2fa98b
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-06-03 20:37:37 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -2,4 +2,11 @@
 ## Imprima el promedio de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
-
+import pandas as pd
+import numpy as np
+pd.set_option('display.notebook_repr_html', False)
+tbl_0 = pd.read_csv('tbl0.tsv',
+                   sep = '\t',
+                   thousands = None,
+                   decimal = '.')
+tbl_0.groupby('_c1').mean()[['_c2']]
